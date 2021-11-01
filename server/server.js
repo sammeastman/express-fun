ec9d6ebf6297cbf8f1a23a74ff915a677d7fc3e3
IB_Student <student@innovatbhamsmbp.attlocal.net>
2021-11-01 13:56:58 -0500

    almost

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+let app = express();
+
+app.use((req, res, next) => {
+    console.log(req.originalUrl);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+
+// app.get('/order/:name', (req, res) => {
+//     let name = req.params.name;
+//     let email = req.query.email;
+//     res.send(`Your name is ${name} and email is ${email}`);
+// });
+
+app.listen(3000);
